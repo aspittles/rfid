91eef0436b5368f5224a3e2bbacd0097768a498e
Adrian Spittles <adrian@spittles.com>
2021-12-06 21:38:50 +1100

    Added support menu

---
+++ menu.sh
@@ -0,0 +1,13 @@
+rfid
+clear
+sh sysinfo.sh
+echo "    Door Lock Service"
+echo "    1. Status"
+echo "    2. Restart"
+echo "    3. Start"
+echo "    4. Stop"
+echo
+echo "    5. Add New User"
+echo
+echo "    0. This Menu"
+echo
