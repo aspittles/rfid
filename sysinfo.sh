9ed271a91be5bd9931e9ce4721d276384917fd8e
Adrian Spittles <adrian@spittles.com>
2021-12-10 13:40:22 +1100

    Updated Menu and SysInfo

--- sysinfo.sh
+++ sysinfo.sh
@@ -11,14 +11,15 @@ UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
 read one five fifteen rest < /proc/loadavg
 
 echo "$(tput setaf 2)
-   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
-  '. \ ' ' / .'   `uname -srmo`$(tput setaf 1)
+                  `date +"%A, %e %B %Y, %r"`
+   .~~.   .~~.    `uname -srmo`
+  '. \ ' ' / .'   `lsb_release -d | sed "s/Description:\t//"`$(tput setaf 1)
    .~ .~~~..~.
   : .~.'~'.~. :   Uptime.............: ${UPTIME}
  ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
 ( : '~'.~.'~' : ) Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
  ~ .~ (   ) ~. ~  Running Processes..: `ps ax | wc -l | tr -d " "`
   (  : '~' :  )   IP Addresses.......: `ip a | grep glo | awk '{print $2}' | head -1 | cut -f1 -d/` and `wget -q -O - http://icanhazip.com/ | tail`
-   '~ .~~~. ~'    CPU Temprature.....: `/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//'`
+   '~ .~~~. ~'    CPU Temprature.....: `vcgencmd measure_temp | sed 's/temp=//'`
        '~'
 $(tput sgr0)"
