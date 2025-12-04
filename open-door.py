07981a8bcf2e114eeafe6ef2076b75183dfbd7e5
Adrian Spittles <adrian@spittles.com>
2025-12-04 20:34:54 +1100

    Added Python Script to Simply Open the Door

---
+++ open-door.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import sys, logging, json, datetime, requests, subprocess
+from time import sleep
+from modules import *
+
+# Read the config file and store in memory
+with open('/home/pi/rfid/rfid-door-lock.json') as f:
+  data = json.load(f)
+
+# Enable & configure logging
+logging.basicConfig(filename=(data["config"]["log_file"]),level=logging.INFO,format='%(asctime)s %(levelname)s:%(message)s')
+
+# Setup GPIO Pins for use with Bi-Colour LED & Mosfet Power Switch Module
+gpio_init()
+
+temp = get_temp()
+led_green()
+logging.info("ALLOW: Remote Door Open")
+logging.info("Raspberry Pi Temp: " + str(temp))
+mosfet_on() # Send 12V power to door Solenoid to open door
+sleep(5);
+led_off()
+mosfet_off()
