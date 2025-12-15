41a78116d58a117eaf3e43f94ca7e04a1c210ee2
Adrian Spittles <adrian@spittles.com>
2025-12-15 21:12:45 +1100

    Add open-door.py for remote door control
    Implement door opening functionality with logging and GPIO control.

---
+++ python-testing/open-door.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import sys, logging, json, datetime, requests, subprocess, os
+from time import sleep
+
+# Base application directory
+APP_DIR = "/opt/rfid-door-lock"
+
+sys.path.append(APP_DIR)
+from modules import *
+
+# Read the config file and store in memory
+with open(os.path.join(APP_DIR, 'config/rfid-door-lock.json')) as f:
+  data = json.load(f)
+
+# Enable & configure logging
+logging.basicConfig(filename=(data["config"]["log_file"]), level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+# Setup GPIO Pins for use with Bi-Colour LED & Mosfet Power Switch Module
+gpio_init()
+
+temp = get_temp()
+led_green() # Show the Green Light to indicate Door is open
+logging.info("ALLOW: Remote Door Open")
+logging.info("Raspberry Pi Temp: " + str(temp))
+print("**** Door Open ****")
+print("This window will auto close in 5 Seconds")
+mosfet_on() # Send 12V power to door Solenoid to open door
+sleep(5) # Keep door open for 5 sec
+led_off()
+mosfet_off() # Stop 12V power to door Solenoid to close door
